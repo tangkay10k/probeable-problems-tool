393044c80becbd91161a49e80c72fe38e05aac5f
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-05-04 16:36:02 +1200

    Completed mvp of java marker class

---
+++ markers/java/Foo.java
@@ -0,0 +1,7 @@
+public class Foo {
+
+    public int foo() {
+        System.out.println("FOO");
+        return 10;
+    }
+}
---
+++ markers/java/FooTest.java
@@ -0,0 +1,13 @@
+public class FooTest {
+
+    int testFoo() {
+        Foo foo = new Foo();
+        return foo.foo();
+    }
+
+    void testCapture() {
+        int a = 5;
+        System.out.println("SIU");
+    }
+}
+
