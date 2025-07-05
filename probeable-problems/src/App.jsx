8a3e909f8dd81874988ae7eb5e1d5facb6341b75
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-07-05 15:22:29 +1200

    create withLoading hook and refactor components

--- probeable-problems/src/App.jsx
+++ probeable-problems/src/App.jsx
@@ -1,19 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/home'
-import QuestionSetup from './pages/question-setup/question-setup'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
+import QuestionSetup from "./pages/question-setup/question-setup";
 import NavBar from "@/components/nav/navbar.jsx";
-import "./App.css"
+import "./App.css";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/setup" element={<QuestionSetup />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
