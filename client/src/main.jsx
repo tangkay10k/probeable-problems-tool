3211d0bdc64f04d6afbbbe934d589432586d5aaa
Hosea Tong-Ho <hton892@aucklanduni.ac.nz>
2025-07-26 12:46:45 +1200

    test login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="694933505323-ul2k8fm8tv2q36p1k6m341jenunces2v.apps.googleusercontent.com">
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </GoogleOAuthProvider>,
 );
