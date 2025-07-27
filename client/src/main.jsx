e9db29e945c24bd08653772d86872811a686cc51
Hosea Tong-Ho <hton892@aucklanduni.ac.nz>
2025-07-27 22:09:34 +1200

    constants

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from "@/constants/google-constants.js";
 
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="694933505323-ul2k8fm8tv2q36p1k6m341jenunces2v.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <StrictMode>
       <BrowserRouter>
         <App />
