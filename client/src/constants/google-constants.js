2a229f33aba77b5ab636ad766b425f0c400c41b5
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-07-29 19:00:17 +1200

    Revert

--- client/src/constants/google-constants.js
+++ client/src/constants/google-constants.js
@@ -1,1 +1,2 @@
-export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_O_AUTH_CLIENT_ID;
+export const GOOGLE_CLIENT_ID =
+  "837455747674-5n70nho4dfur8ah1dqja3om3cho76log.apps.googleusercontent.com";
