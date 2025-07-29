528b001e0b8818f180c5af5529b1d2ace31b6017
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-07-29 18:14:15 +1200

    Update oAuth client

--- client/src/constants/google-constants.js
+++ client/src/constants/google-constants.js
@@ -1,2 +1,4 @@
 export const GOOGLE_CLIENT_ID =
-  "694933505323-ul2k8fm8tv2q36p1k6m341jenunces2v.apps.googleusercontent.com";
+  "837455747674-5n70nho4dfur8ah1dqja3om3cho76log.apps.googleusercontent.com";
+
+// OLD "694933505323-ul2k8fm8tv2q36p1k6m341jenunces2v.apps.googleusercontent.com";
