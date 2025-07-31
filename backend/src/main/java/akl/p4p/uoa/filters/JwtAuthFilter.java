62b37f382722770051d12874d3b41d622278c3a5
Hosea Tong-Ho <hton892@aucklanduni.ac.nz>
2025-07-31 15:22:02 +1200

    lint

--- backend/src/main/java/akl/p4p/uoa/filters/JwtAuthFilter.java
+++ backend/src/main/java/akl/p4p/uoa/filters/JwtAuthFilter.java
@@ -9,8 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -18,7 +16,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -30,30 +27,30 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     this.authTokenService = authTokenService;
     this.jwtService = jwtService;
   }
-@Override
-protected void doFilterInternal(
-    HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-    throws ServletException, IOException {
-  try {
-    String authHeader = request.getHeader(AuthConstants.P4P_AUTH_HEADER);
 
-    if (authHeader != null && authHeader.startsWith(AuthConstants.BEARER_PREFIX)) {
-      String token = authHeader.substring(AuthConstants.BEARER_PREFIX.length());
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      String authHeader = request.getHeader(AuthConstants.P4P_AUTH_HEADER);
 
-      authTokenService.verifyAuthToken(token);
-      String role = jwtService.extractRole(token);
+      if (authHeader != null && authHeader.startsWith(AuthConstants.BEARER_PREFIX)) {
+        String token = authHeader.substring(AuthConstants.BEARER_PREFIX.length());
 
-      List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
-      Authentication auth = new UsernamePasswordAuthenticationToken(null, null, authorities);
-      SecurityContextHolder.getContext().setAuthentication(auth);
+        authTokenService.verifyAuthToken(token);
+        String role = jwtService.extractRole(token);
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + role));
+        Authentication auth = new UsernamePasswordAuthenticationToken(null, null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+      }
+
+      filterChain.doFilter(request, response);
+    } catch (Exception e) {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.getWriter().write("Unauthorized");
+      response.getWriter().flush();
     }
-
-    filterChain.doFilter(request, response); 
-  } catch (Exception e) {
-    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    response.getWriter().write("Unauthorized");
-    response.getWriter().flush();
   }
-}
-
 }
