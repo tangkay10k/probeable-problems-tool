ef3b005bf1219ae940a14c91dcae3649810153e0
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-08-01 14:45:17 +1200

    reduce stage 1 text

--- client/src/pages/problem/data/instructions.js
+++ client/src/pages/problem/data/instructions.js
@@ -2,17 +2,15 @@ export const STAGE_ONE = [
   {
     title: "Tasks",
     content:
-      "You are a software engineer and your client has asked you to develop some software for them! They don’t come" +
-      " from a technical background so you will need to ask them for more information! \n" +
+      "You are a software engineer. \n" +
       "\n" +
-      "Be sure that you clarify with them carefully about what they want to ensure that they they are satisfied with" +
-      " your solution.\n" +
+      "Your client needs software developed but lacks a technical background, so you must ask clear, careful questions to understand their exact requirements.\n" +
       "\n" +
-      "You may find keeping track of your findings on a notepad helpful - Also ensures you maintain professionalism and stops you from asking the client questions they have already answered!\n" +
+      "Your client has also provided the binary of similar software. You can test this binary" +
+      " with" +
+      " different inputs to determine functionality.\n" +
       "\n" +
-      "Somehow, they have managed to acquire the binary to the software but they do not have access to the source code. You may test the software’s expected output with different input values to further understand what you have to write!\n" +
-      "\n" +
-      "Once you are confident you know what exactly the client wants you can proceed to next page. \n",
+      "Once you fully understand what the client wants, proceed to the next page.",
   },
 ];
 
