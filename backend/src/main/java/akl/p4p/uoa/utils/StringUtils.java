3081d28f9314f3301e8958b0a03fab2b063c8c3d
SirBillyBobJoe <hton892@aucklanduni.ac.nz>
2025-08-21 16:40:29 +1200

    comments

--- backend/src/main/java/akl/p4p/uoa/utils/StringUtils.java
+++ backend/src/main/java/akl/p4p/uoa/utils/StringUtils.java
@@ -17,22 +17,6 @@ public class StringUtils {
   }
 
   public static String extractFunctionSignature(String code) {
-    String regex =
-        "(?s)"
-            + "\\b(?!if\\b|for\\b|while\\b|switch\\b)"
-            + "([A-Za-z_][\\w\\s]*?)\\s*"
-            + "((?:\\*\\s*)*)\\s*"
-            + "([A-Za-z_]\\w*)\\s*"
-            + "\\(([^;{}]*)\\)\\s*\\{";
-
-    Matcher m = Pattern.compile(regex, Pattern.DOTALL).matcher(code);
-    if (!m.find()) return null;
-
-    String ret = m.group(1).replaceAll("\\s+", " ").trim();
-    String stars = m.group(2).replaceAll("\\s+", "");
-    String name = m.group(3);
-    String params = m.group(4).replaceAll("\\s*,\\s*", ", ").replaceAll("\\s+", " ").trim();
-
-    return ret + " " + stars + name + "(" + params + ")";
+    return code.split("\\{")[0];
   }
 }
