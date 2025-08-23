39783104a96a3b4d6e965d1b439bbb3422a8dd4b
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-08-23 15:13:33 +1200

    Minor ui tweaks

--- client/src/components/nav/navbar.jsx
+++ client/src/components/nav/navbar.jsx
@@ -16,15 +16,16 @@ export default function NavBar() {
 
   return (
     <nav className={styles.navbar}>
-      <span className={styles.leftSection}>
-        <section onClick={() => navigate("/problems")}>
-          <img className={styles.icon} src={"/favicon.png"} alt="Logo" />
-          <div className={styles.heading}>
-            <h1>Probeable Problems</h1>
-            <p>Developing Critical Thinking</p>
-          </div>
-        </section>
-      </span>
+      <section
+        onClick={() => navigate("/problems")}
+        className={styles.leftSection}
+      >
+        <img className={styles.icon} src={"/favicon.png"} alt="Logo" />
+        <div className={styles.heading}>
+          <h1>Probeable Problems</h1>
+          <p>Developing Critical Thinking</p>
+        </div>
+      </section>
       {!isLoginScreen ? (
         <section>
           <CircularIconButton
