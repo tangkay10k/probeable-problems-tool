e2e31f328bc58fbeb480accbab0ca90f34d56f7c
Hosea Tong-Ho <hton892@aucklanduni.ac.nz>
2025-08-27 17:16:27 +1200

    testing

--- client/src/context/logging-context-provider.jsx
+++ client/src/context/logging-context-provider.jsx
@@ -14,6 +14,7 @@ export function LoggingProvider({ children }) {
   
   const addLog = useCallback(
     (entry) => {
+      console.log(import.meta.env.PROD)
       if (!LOGGING_ENABLED) {
         return;
       }
