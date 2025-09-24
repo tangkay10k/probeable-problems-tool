62fbc9a312bf97b2843524f32500150dbb93c56e
Kay Tang <126426690+ktan185@users.noreply.github.com>
2025-09-24 19:23:47 +1200

    Side quest: update instructions

--- client/src/pages/problem/data/instructions.js
+++ client/src/pages/problem/data/instructions.js
@@ -33,9 +33,10 @@ export const STAGE_ONE_NATURAL_LANGUAGE = {
 export const STAGE_ONE_EXAMPLE = {
   title: "Example (not marked)",
   content:
-    "Welcome! This is just an example and will not be marked.\n\n" +
-    "For this example, you have access to both the **Client** and the **Run** tab. \n\n" +
-    "You can clarify what the program should do by asking the client questions, or by testing it with various inputs using the **Run** tab.\n\n" +
+    "Welcome! This is just an example question and will not be marked. Play around and understand how to use the" +
+    " application!\n\n" +
+    "You can clarify what the you need to do should do by asking the client questions, or by testing it with various" +
+    " inputs using the **Run** tab.\n\n" +
     "Once you are confident that you understand what the client wants, click **I'm ready to code!**",
 };
 
@@ -51,8 +52,8 @@ export const STAGE_ONE_CONCISE_FULL = {
 export const STAGE_ONE_CONCISE_EXAMPLE = {
   title: "Tasks",
   content:
-    "## Welcome! 👋\nThis is your first task and is an example of what you should expect for following tasks.\n\n" +
-    "**Note:** This is an example scenario and is **not marked**. It also allows access to both the **Client** and the **Run** tab.\n\n" +
+    "## Welcome! 👋\nThis is a tutorial question for you to familiarise yourself with the application.\n\n" +
+    "**This question is not marked**.\n\n" +
     "I want you to **//VAR_PROBLEM_STATEMENT**\n\n" +
     "Once you’re confident in the requirements, proceed to implement your solution.",
 };
